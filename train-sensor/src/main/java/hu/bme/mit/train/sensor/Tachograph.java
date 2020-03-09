fbc0dfba11d882a5fe649875358db2ff545a1568
awrpad <wgnrar@gmail.com>
2020-03-09 16:40:29 +0100

    Implemented Tachograph

---
+++ train-sensor/src/main/java/hu/bme/mit/train/sensor/Tachograph.java
@@ -0,0 +1,35 @@
+package hu.bme.mit.train.sensor;
+
+import com.google.common.collect.Table;
+import hu.bme.mit.train.interfaces.TrainController;
+import hu.bme.mit.train.interfaces.TrainUser;
+
+import java.util.Collection;
+import java.util.Date;
+
+public class Tachograph {
+    private static  String JOYSTICK_COLUMN_NAME = "JoyStick";
+    private static  String REFERENCE_SPEED_COLUMN_NAME = "RefSpeed";
+    private Table<Date, String, Integer> table;
+    private TrainUser user;
+    private TrainController controller;
+
+    public Tachograph(TrainUser user, TrainController controller){
+        this.user = user;
+        this.controller = controller;
+    }
+    
+    public void document(){
+        Date currentDate = new Date();
+        table.put(currentDate, JOYSTICK_COLUMN_NAME, user.getJoystickPosition());
+        table.put(currentDate, REFERENCE_SPEED_COLUMN_NAME, controller.getReferenceSpeed());
+    }
+
+    public Collection<Integer> getSpeedValues(){
+        return table.column(REFERENCE_SPEED_COLUMN_NAME).values();
+    }
+
+    public Collection<Integer> getJoystickValues(){
+        return table.column(JOYSTICK_COLUMN_NAME).values();
+    }
+}
