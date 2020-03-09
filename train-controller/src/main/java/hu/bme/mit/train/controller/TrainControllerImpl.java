5695b23f9dc114f80b5de26889f529c6c0616e35
awrpad <wgnrar@gmail.com>
2020-03-09 15:58:13 +0100

    Fixed everything

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -15,13 +15,13 @@ public class TrainControllerImpl implements TrainController {
 			referenceSpeed = 0;
 		} else {
 		    if(referenceSpeed+step > 0) {
-                referenceSpeed += step;
+                referenceSpeed = step;
             } else {
 		        referenceSpeed = 0;
             }
 		}
 
-		enforceSpeedLimit();
+		//enforceSpeedLimit();
 	}
 
 	@Override
