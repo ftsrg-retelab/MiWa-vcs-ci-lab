449a684faba8a779497d3f1e55eba7c937b62784
awrpad <wgnrar@gmail.com>
2020-04-03 07:20:22 +0200

    Implemented usage of the joystick

--- train-user/src/main/java/hu/bme/mit/train/user/TrainUserImpl.java
+++ train-user/src/main/java/hu/bme/mit/train/user/TrainUserImpl.java
@@ -3,13 +3,32 @@ package hu.bme.mit.train.user;
 import hu.bme.mit.train.interfaces.TrainController;
 import hu.bme.mit.train.interfaces.TrainUser;
 
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class TrainUserImpl implements TrainUser {
 
 	private TrainController controller;
 	private int joystickPosition;
 
 	public TrainUserImpl(TrainController controller) {
-		this.controller = controller;
+	    this.controller = controller;
+	    Timer t = new Timer();
+	    Random r = new Random();
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run()
+            {
+                int ran = r.nextInt(10);
+                int joysickNewPos = ran - 9;
+
+                overrideJoystickPosition(joysickNewPos);
+            }
+        };
+
+        // After started, wait 3.5s then execute timerTask every second
+        t.scheduleAtFixedRate(timerTask, 3500, 1000);
 	}
 
 	@Override
